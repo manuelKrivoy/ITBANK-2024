3d199dffe890b0862ea7c35be0e14b792119b78a
Manuel Krivoy <krivoymanuel@gmail.com>
2024-09-16 11:04:57 -0300

    mejoro rendimiento cache

--- src/app/profile/layout.jsx
+++ src/app/profile/layout.jsx
@@ -3,13 +3,16 @@ import "jsvectormap/dist/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "./style.css";
 import React from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
-      <body suppressHydrationWarning={true}>
-        <div>{children}</div>
-      </body>
-    </html>
+    <AppRouterCacheProvider>
+      <html lang="es">
+        <body suppressHydrationWarning={true}>
+          <div>{children}</div>
+        </body>
+      </html>
+    </AppRouterCacheProvider>
   );
 }

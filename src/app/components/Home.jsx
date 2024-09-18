a7e9e2bc5a6f8892bb58c6b423e8c6abcee0ab28
Fran Ruslender <fruslender@gmail.com>
2024-09-18 19:12:48 -0300

    update de optimizacion de <Image> en Home.jsx

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -1,11 +1,48 @@
+// import styles from "./home.module.css";
+// import Link from "next/link";
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <div className={styles.content}>
+//         <img className={styles.logo} src="./logo.svg" alt="logo" />
+//         <Link href="/login" prefetch={true}>
+//           <button className={styles.seParte}>Forma parte</button>
+//         </Link>
+//       </div>
+
+//       <div className={`${styles.light} ${styles.x1}`}></div>
+//       <div className={`${styles.light} ${styles.x2}`}></div>
+//       <div className={`${styles.light} ${styles.x3}`}></div>
+//       <div className={`${styles.light} ${styles.x4}`}></div>
+//       <div className={`${styles.light} ${styles.x5}`}></div>
+//       <div className={`${styles.light} ${styles.x6}`}></div>
+//       <div className={`${styles.light} ${styles.x7}`}></div>
+//       <div className={`${styles.light} ${styles.x8}`}></div>
+//       <div className={`${styles.light} ${styles.x9}`}></div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+import React from "react";
 import styles from "./home.module.css";
 import Link from "next/link";
+import Image from 'next/image'; // Importa el componente Image
 
 const Home = () => {
   return (
     <div>
       <div className={styles.content}>
-        <img className={styles.logo} src="./logo.svg" alt="logo" />
+        <Image 
+          className={styles.logo} 
+          src="/logo.svg" // Usa la ruta absoluta desde la carpeta public
+          alt="logo" 
+          width={200} // Define el ancho de la imagen
+          height={100} // Define la altura de la imagen
+        />
         <Link href="/login" prefetch={true}>
           <button className={styles.seParte}>Forma parte</button>
         </Link>

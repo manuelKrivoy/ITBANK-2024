6b24aab18e3a592d2555f4ab03048933979c2511
Manuel Krivoy <krivoymanuel@gmail.com>
2024-10-14 14:57:06 -0300

    empiezo a aplicar herencia en clase Cliente

--- backend/sprint_v/clases/__init__.py
+++ backend/sprint_v/clases/__init__.py
@@ -1,4 +1,4 @@
-from .cliente import Cliente
+from .cliente import Cliente, ClienteClassic, ClienteGold, ClienteBlack
 from .cuenta import Cuenta
 from .transaccion import Transaccion
 from .modulo_calculos import ModuloCalculos
--- backend/sprint_v/clases/cliente.py
+++ backend/sprint_v/clases/cliente.py
@@ -14,3 +14,53 @@ class Cliente:
 
     def agregar_transaccion(self, transaccion):
         self.transacciones.append(transaccion)
+
+    def retirarDinero(self, cuenta, monto):
+        pass
+
+
+## Defino clases para el tipo de cliente utilizando herencia
+class ClienteClassic(Cliente):
+    def __init__(self, numero, nombre, apellido, dni):
+        super().__init__(numero, nombre, apellido, dni, 'Classic')
+
+    def retirarDinero(self, cuenta, monto):
+        if monto > 10000:
+            if cuenta.saldo >= monto:
+                cuenta.saldo -= monto
+                cuenta.saldo -= cuenta.comision
+                return True
+            return False
+        else:
+            print("No puede retirar más de $10000")
+
+
+class ClienteGold(Cliente):
+    def __init__(self, numero, nombre, apellido, dni):
+        super().__init__(numero, nombre, apellido, dni, 'Gold')
+    
+    def retirarDinero(self, cuenta, monto):
+        if monto > 20000:
+            if cuenta.saldo >= monto:
+                cuenta.saldo -= monto
+                cuenta.saldo -= cuenta.comision
+                return True
+            return False
+        else:
+            print("No puede retirar más de $20000")    
+
+
+class ClienteBlack(Cliente):
+    def __init__(self, numero, nombre, apellido, dni):
+        super().__init__(numero, nombre, apellido, dni, 'Black')
+    
+    def retirarDinero(self, cuenta, monto):
+        if monto > 100000:
+            if cuenta.saldo >= monto:
+                cuenta.saldo -= monto
+                cuenta.saldo -= cuenta.comision
+                return True
+            return False
+        else:
+            print("No puede retirar más de $100000")        
+

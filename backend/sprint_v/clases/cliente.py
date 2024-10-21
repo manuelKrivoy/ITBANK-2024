a90ac61b2dbb359e55ddeb3c03facec708385f6f
Manuel Krivoy <krivoymanuel@gmail.com>
2024-10-20 21:40:47 -0300

    reset

--- backend/sprint_v/clases/cliente.py
+++ backend/sprint_v/clases/cliente.py
@@ -1,187 +1,4 @@
-# from datetime import datetime
-
-# class Cliente:
-#     def __init__(self, numero, nombre, apellido, dni, tipo, tarjetaDebito, direccion, cajaAhorroPesos):
-#         self.numero = numero
-#         self.nombre = nombre
-#         self.apellido = apellido
-#         self.dni = dni
-#         self.tipo = tipo
-#         self.tarjetaDebito = tarjetaDebito
-#         self.cuentas = []
-#         self.transacciones = []
-#         self.direccion = direccion
-#         self.cajaDeAhorroPesos = cajaAhorroPesos
-#         self.autorizacion = False
-#         self.montoRetiradoHoy = 0  # Registro de cuánto ha retirado hoy
-#         self.fechaUltimoRetiro = datetime.now().date()  # Fecha del último retiro
-
-#     def __str__(self):
-#         return f"{self.nombre} {self.apellido} ({self.tipo})"  
-        
-#     def agregar_cuenta(self, cuenta):
-#         self.cuentas.append(cuenta)
-
-#     def agregar_transaccion(self, transaccion):
-#         self.transacciones.append(transaccion)
-
-#     def resetear_monto_diario(self):
-#         if self.fechaUltimoRetiro != datetime.now().date():
-#             self.montoRetiradoHoy = 0
-#             self.fechaUltimoRetiro = datetime.now().date()
-
-#     def puede_retirar(self, monto, limiteDiario):
-#         if monto < 0:
-#             return False, "No puede retirar un monto negativo"
-#         else:
-#             self.resetear_monto_diario()
-#             if self.montoRetiradoHoy + monto > limiteDiario:
-#                 return False, f"No puede retirar más de ${limiteDiario} por día."
-#             return True, ""
-
-#     def registrar_retiro(self, monto):
-#         self.montoRetiradoHoy += monto
-
-#     # Método generalizado para retirar dinero en todas las subclases
-#     def retirar_dinero(self, cuenta, monto, limiteDiario):
-#         self.resetear_monto_diario()
-#         puede_retirar, mensaje = self.puede_retirar(monto, limiteDiario)
-#         if not puede_retirar:
-#             return mensaje
-#         else:
-#             if cuenta.saldo >= monto:
-#                 cuenta.saldo -= monto
-#                 self.registrar_retiro(monto)
-#                 return 1
-#             else:
-#                 return "Saldo insuficiente"
-
-#     def realizar_transferencia(self, clienteDestino, monto):
-#         # Calcular el monto final con la comisión
-#         montoConComision = monto * self.comision
-
-#         limites_autorizacion = {
-#             'Classic': 150000,
-#             'Gold': 500000,
-#             'Black': None  # Sin límite para cuentas Black
-#         }
-
-#         limite = limites_autorizacion.get(self.tipo)
-
-#         # Verificar si la transferencia requiere autorización
-#         if limite is not None and monto > limite and not self.autorizacion:
-#             return "La cuenta destino no ha autorizado la transferencia."
-#         if self.cajaDeAhorroPesos.saldo < montoConComision:
-
-#             return "Saldo insuficiente."
-
-#         # Ejecutar la transferencia
-#         self.cajaDeAhorroPesos.saldo -= montoConComision
-#         # Se transfiere monto sin comisión
-#         clienteDestino.cajaDeAhorroPesos.saldo += monto  
-
-
-#         return 1
-
-#     def alta_tarjeta_credito(self, tarjetaCredito):
-#         pass
-
-#     # Métodos para comprar y vender dólares
-#     def vender_usd(self, monto):
-#         if hasattr(self, 'cajaAhorroDolares') and self.cajaAhorroDolares.saldo >= monto:
-#             self.cajaAhorroDolares.saldo -= monto
-#             print(f"Se han vendido ${monto} USD con éxito.")
-#         else:
-#             print("Saldo insuficiente en caja de ahorro en dólares.")
-
-#     def comprar_usd(self, monto):
-#         if hasattr(self, 'cajaAhorroDolares'):
-#             self.cajaAhorroDolares.saldo += monto
-#             print(f"Se han comprado ${monto} USD con éxito.")
-#         else:
-#             print("No tiene cuenta en dólares.")
-
-# # Clases de tipo de cliente heredando de Cliente
-# class ClienteClassic(Cliente):
-#     LIMITE_DIARIO = 10000
-
-#     def __init__(self, numero, nombre, apellido, dni, tarjetaDebito, direccion, cajaAhorroPesos):
-#         super().__init__(numero, nombre, apellido, dni, 'Classic', tarjetaDebito, direccion, cajaAhorroPesos)
-#         self.comision = 1.01
-    
-#     def aprobar_desaprobar_transferencia(self):
-#         self.autorizacion = not self.autorizacion
-
-#     def retirar_dinero(self, cuenta, monto):
-#         return super().retirar_dinero(cuenta, monto, self.LIMITE_DIARIO)
-
-#     def alta_tarjeta_credito(self, tarjetaCredito):
-#         return "No puede tener tarjeta de crédito siendo cliente Classic"
-
-# class ClienteGold(Cliente):
-#     LIMITE_DIARIO = 20000
-
-#     def __init__(self, numero, nombre, apellido, dni, tarjetaDebito, direccion, cajaAhorroPesos, cajaAhorroDolares):
-#         super().__init__(numero, nombre, apellido, dni, 'Gold', tarjetaDebito, direccion, cajaAhorroPesos)
-#         self.cajaAhorroDolares = cajaAhorroDolares
-#         self.tarjetaCredito = None
-#         self.comision = 1.005
-
-#     def aprobar_desaprobar_transferencia(self):
-#         self.autorizacion = not self.autorizacion
-
-#     def retirar_dinero(self, cuenta, monto):
-#         return super().retirar_dinero(cuenta, monto, self.LIMITE_DIARIO)
-
-#     def alta_tarjeta_credito(self, tarjetaCredito):
-#         if self.tarjetaCredito is None:
-#             self.tarjetaCredito = tarjetaCredito
-#             return 1
-#         else:
-#             return "Ya tiene una tarjeta de crédito"
-
-#     def eliminar_tarjeta_credito(self):
-#         if self.tarjetaCredito is not None:
-#             self.tarjetaCredito = None
-#         else:
-#             print("No tiene tarjeta de crédito")
-
-# class ClienteBlack(Cliente):
-#     LIMITE_DIARIO = 100000
-
-#     def __init__(self, numero, nombre, apellido, dni, tarjetaDebito, direccion, cajaAhorroPesos, cajaAhorroDolares):
-#         super().__init__(numero, nombre, apellido, dni, 'Black', tarjetaDebito, direccion, cajaAhorroPesos)
-#         self.cajaAhorroDolares = cajaAhorroDolares
-#         self.tarjetaCredito = []
-#         self.comision = 1
-#         self.autorizacion = True
-
-#     def retirar_dinero(self, cuenta, monto):
-#         return super().retirar_dinero(cuenta, monto, self.LIMITE_DIARIO)
-
-#     def agregar_tarjeta_credito(self, tarjetaCredito):
-#         if len(self.tarjetaCredito) < 5:
-#             self.tarjetaCredito.append(tarjetaCredito)
-#             return 1
-#         else:
-#             return "No puede tener más de 5 tarjetas de crédito"
-
-#     def eliminar_tarjeta_credito(self, tarjetaCredito):
-#         if tarjetaCredito in self.tarjetaCredito:
-#             self.tarjetaCredito.remove(tarjetaCredito)
-#         else:
-#             print("No tiene esa tarjeta de crédito")
-
 from datetime import datetime
-
-class InsufficientFundsError(Exception):
-    pass
-
-class DailyLimitExceededError(Exception):
-    pass
-
-class ClientNotAuthorizedError(Exception):
-    pass
 
 class Cliente:
     def __init__(self, numero, nombre, apellido, dni, tipo, tarjetaDebito, direccion, cajaAhorroPesos):
@@ -215,33 +32,76 @@ class Cliente:
 
     def puede_retirar(self, monto, limiteDiario):
         if monto < 0:
-            raise ValueError("No puede retirar un monto negativo")
-        
-        self.resetear_monto_diario()
-        
-        if self.montoRetiradoHoy + monto > limiteDiario:
-            raise DailyLimitExceededError(f"No puede retirar más de ${limiteDiario} por día.")
-        
-        return True
+            return False, "No puede retirar un monto negativo"
+        else:
+            self.resetear_monto_diario()
+            if self.montoRetiradoHoy + monto > limiteDiario:
+                return False, f"No puede retirar más de ${limiteDiario} por día."
+            return True, ""
 
     def registrar_retiro(self, monto):
         self.montoRetiradoHoy += monto
 
-    def retirar_dinero(self, cuenta, monto):
+    # Método generalizado para retirar dinero en todas las subclases
+    def retirar_dinero(self, cuenta, monto, limiteDiario):
         self.resetear_monto_diario()
-        
-        if not self.puede_retirar(monto, self.LIMITE_DIARIO): 
-            return
-        
-        if cuenta.saldo >= monto:
-            cuenta.saldo -= monto
-            self.registrar_retiro(monto)
-            return 1
-        
-        raise InsufficientFundsError("Saldo insuficiente")
+        puede_retirar, mensaje = self.puede_retirar(monto, limiteDiario)
+        if not puede_retirar:
+            return mensaje
+        else:
+            if cuenta.saldo >= monto:
+                cuenta.saldo -= monto
+                self.registrar_retiro(monto)
+                return 1
+            else:
+                return "Saldo insuficiente"
 
-# Clases de tipo de cliente heredando de Cliente (con excepciones)
+    def realizar_transferencia(self, clienteDestino, monto):
+        # Calcular el monto final con la comisión
+        montoConComision = monto * self.comision
 
+        limites_autorizacion = {
+            'Classic': 150000,
+            'Gold': 500000,
+            'Black': None  # Sin límite para cuentas Black
+        }
+
+        limite = limites_autorizacion.get(self.tipo)
+
+        # Verificar si la transferencia requiere autorización
+        if limite is not None and monto > limite and not self.autorizacion:
+            return "La cuenta destino no ha autorizado la transferencia."
+        if self.cajaDeAhorroPesos.saldo < montoConComision:
+
+            return "Saldo insuficiente."
+
+        # Ejecutar la transferencia
+        self.cajaDeAhorroPesos.saldo -= montoConComision
+        # Se transfiere monto sin comisión
+        clienteDestino.cajaDeAhorroPesos.saldo += monto  
+
+
+        return 1
+
+    def alta_tarjeta_credito(self, tarjetaCredito):
+        pass
+
+    # Métodos para comprar y vender dólares
+    def vender_usd(self, monto):
+        if hasattr(self, 'cajaAhorroDolares') and self.cajaAhorroDolares.saldo >= monto:
+            self.cajaAhorroDolares.saldo -= monto
+            print(f"Se han vendido ${monto} USD con éxito.")
+        else:
+            print("Saldo insuficiente en caja de ahorro en dólares.")
+
+    def comprar_usd(self, monto):
+        if hasattr(self, 'cajaAhorroDolares'):
+            self.cajaAhorroDolares.saldo += monto
+            print(f"Se han comprado ${monto} USD con éxito.")
+        else:
+            print("No tiene cuenta en dólares.")
+
+# Clases de tipo de cliente heredando de Cliente
 class ClienteClassic(Cliente):
     LIMITE_DIARIO = 10000
 
@@ -249,11 +109,14 @@ class ClienteClassic(Cliente):
         super().__init__(numero, nombre, apellido, dni, 'Classic', tarjetaDebito, direccion, cajaAhorroPesos)
         self.comision = 1.01
     
+    def aprobar_desaprobar_transferencia(self):
+        self.autorizacion = not self.autorizacion
+
     def retirar_dinero(self, cuenta, monto):
-        return super().retirar_dinero(cuenta, monto)
+        return super().retirar_dinero(cuenta, monto, self.LIMITE_DIARIO)
 
     def alta_tarjeta_credito(self, tarjetaCredito):
-        raise Exception("No puede tener tarjeta de crédito siendo cliente Classic")
+        return "No puede tener tarjeta de crédito siendo cliente Classic"
 
 class ClienteGold(Cliente):
     LIMITE_DIARIO = 20000
@@ -264,13 +127,24 @@ class ClienteGold(Cliente):
         self.tarjetaCredito = None
         self.comision = 1.005
 
-    def realizar_transferencia(self, clienteDestino, monto):
-        if self.cajaDeAhorroPesos.saldo >= monto:
-            self.cajaDeAhorroPesos.saldo -= monto
-            clienteDestino.cajaDeAhorroPesos.saldo += monto
+    def aprobar_desaprobar_transferencia(self):
+        self.autorizacion = not self.autorizacion
+
+    def retirar_dinero(self, cuenta, monto):
+        return super().retirar_dinero(cuenta, monto, self.LIMITE_DIARIO)
+
+    def alta_tarjeta_credito(self, tarjetaCredito):
+        if self.tarjetaCredito is None:
+            self.tarjetaCredito = tarjetaCredito
             return 1
         else:
-            raise InsufficientFundsError("Saldo insuficiente para realizar la transferencia.")
+            return "Ya tiene una tarjeta de crédito"
+
+    def eliminar_tarjeta_credito(self):
+        if self.tarjetaCredito is not None:
+            self.tarjetaCredito = None
+        else:
+            print("No tiene tarjeta de crédito")
 
 class ClienteBlack(Cliente):
     LIMITE_DIARIO = 100000
@@ -279,13 +153,21 @@ class ClienteBlack(Cliente):
         super().__init__(numero, nombre, apellido, dni, 'Black', tarjetaDebito, direccion, cajaAhorroPesos)
         self.cajaAhorroDolares = cajaAhorroDolares
         self.tarjetaCredito = []
-        self.comision = 1  # Sin comisiones para clientes Black
+        self.comision = 1
         self.autorizacion = True
 
-    def realizar_transferencia(self, clienteDestino, monto):
-        if self.cajaDeAhorroPesos.saldo >= monto:
-            self.cajaDeAhorroPesos.saldo -= monto
-            clienteDestino.cajaDeAhorroPesos.saldo += monto
+    def retirar_dinero(self, cuenta, monto):
+        return super().retirar_dinero(cuenta, monto, self.LIMITE_DIARIO)
+
+    def agregar_tarjeta_credito(self, tarjetaCredito):
+        if len(self.tarjetaCredito) < 5:
+            self.tarjetaCredito.append(tarjetaCredito)
             return 1
         else:
-            raise InsufficientFundsError("Saldo insuficiente para realizar la transferencia.")
+            return "No puede tener más de 5 tarjetas de crédito"
+
+    def eliminar_tarjeta_credito(self, tarjetaCredito):
+        if tarjetaCredito in self.tarjetaCredito:
+            self.tarjetaCredito.remove(tarjetaCredito)
+        else:
+            print("No tiene esa tarjeta de crédito")
--- backend/sprint_v/main.py
+++ backend/sprint_v/main.py
@@ -1,141 +1,7 @@
-# import json
-# from datetime import datetime
-# from clases import ClienteClassic, ClienteBlack, ClienteGold, Transaccion, CajaAhorroPesos, CajaAhorroDolares
-# import os
-
-# def limpiarpantalla():
-#     os.system('cls' if os.name == 'nt' else 'clear')
-    
-# ## Funcion para ver si el json es valido.
-# def validar_json(file_path):
-#     try:
-#         with open(file_path) as f:
-#             return json.load(f)
-#     except ValueError as e:
-#         print(f"Error en el archivo JSON: {e}")
-#         return None
-
-# # Proceso transacciones del json y las guardo en los clientes.
-# def procesar_transacciones(json_file):
-#     data = validar_json(json_file)
-#     if not data:
-#         return None
-
-#     clientes = {}
-    
-#     # Crear los clientes y asignarles transacciones
-#     for item in data:
-#         cliente_numero = item["numero"]
-#         tipo_cliente = item["tipo"]
-        
-#         # Primero me fijo si el cliente ya existe para no crear uno nuevo
-#         if cliente_numero not in clientes:
-#             ## Si no existe creo al cliente.
-#             if tipo_cliente == "CLASSIC":
-#                 clientes[cliente_numero] = ClienteClassic(
-#                     cliente_numero, item["nombre"], item["apellido"], item["DNI"], "Debito", "Mitre 1234", CajaAhorroPesos(1000000)
-#                 )
-#             elif tipo_cliente == "GOLD":
-#                 clientes[cliente_numero] = ClienteGold(
-#                     cliente_numero, item["nombre"], item["apellido"], item["DNI"], "Debito", "San Martin 456", CajaAhorroPesos(20000), 
-#                     CajaAhorroDolares(5000) 
-#                 )
-#             elif tipo_cliente == "BLACK":
-#                 clientes[cliente_numero] = ClienteBlack(
-#                     cliente_numero, item["nombre"], item["apellido"], item["DNI"], "Debito","Italia 789", CajaAhorroPesos(100000),
-#                      CajaAhorroDolares(10000)
-#                 )
-        
-#         # Procesar transacciones
-#         for trans in item["transacciones"]:
-#             fecha_transaccion = datetime.strptime(trans["fecha"], "%d/%m/%Y %H:%M:%S")
-#             nueva_transaccion = Transaccion(trans["tipo"], trans["monto"], fecha_transaccion, trans["estado"])
-#             # Mediante esa funcion se le asigna estado : aprobado / rechazado
-#             validar_transaccion(clientes[cliente_numero], nueva_transaccion, trans)
-    
-#     return clientes
-
-# # Funcion que le asigna estado a la transaccion
-# def validar_transaccion(cliente, transaccion, data_transaccion):
-#     tipo = transaccion.tipo
-#     monto = transaccion.monto
-    
-#     # Ejemplo de procesamiento de retiros en cajero
-#     if tipo == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO":
-#         cuenta = cliente.cajaDeAhorroPesos 
-#         if cliente.retirar_dinero(cuenta, monto) == 1:
-#             transaccion.estado = "ACEPTADA"
-#         else:
-#             transaccion.estado = "RECHAZADA"
-#             transaccion.razon = cliente.retirar_dinero(cuenta, monto)
-    
-#     # Procesar transferencias
-#     elif tipo == "TRANSFERENCIA_ENVIADA":
-#         cliente_destino = buscar_cliente_destino(data_transaccion["cuentaDestino"], cliente)  
-#         if cliente.realizar_transferencia(cliente_destino, monto) == 1:
-#             transaccion.estado = "ACEPTADA"
-#         else:
-#             transaccion.estado = "RECHAZADA"
-#             transaccion.razon = cliente.realizar_transferencia(cliente_destino, monto)
-    
-#     ## Procesar altas de tarjetas
-#     elif tipo == "ALTA_TARJETA_CREDITO":
-#         if cliente.alta_tarjeta_credito(data_transaccion["numeroTarjeta"]) == 1:
-#             transaccion.estado = "ACEPTADA"
-#         else:
-#             transaccion.estado = "RECHAZADA"
-#             transaccion.razon = cliente.alta_tarjeta_credito(data_transaccion["numeroTarjeta"])
-    
-#     cliente.agregar_transaccion(transaccion)
-
-# # Ejemplo de función para buscar el cliente destino en las transferencias
-# def buscar_cliente_destino(numero_cuenta, cliente):
-#     # falta implementar logica para que esto funcione.. por ahora se devuelve el mismo cliente
-#     return cliente
-
-# # Generar el reporte HTML
-# def generar_reporte_html(clientes):
-#     with open("reporte.html", "w") as f:
-#         f.write("<html><body><h1>Reporte de Transacciones</h1><table border='1'>")
-#         f.write("<tr><th>Cliente</th><th>Transaccion</th><th>Estado</th><th>Monto</th><th>Fecha</th><th>Razon</th></tr>")
-        
-#         for cliente in clientes.values():
-#             for trans in cliente.transacciones:
-#                 razon = trans.razon if trans.estado == "RECHAZADA" else ""
-#                 f.write(f"<tr><td>{cliente.nombre} {cliente.apellido}</td><td>{trans.tipo}</td><td>{trans.estado}</td><td>{trans.monto}</td><td>{trans.fecha}</td><td>{razon}</td></tr>")
-        
-#         f.write("</table></body></html>")
-
-# json_file = "transacciones.json"
-
-# clientes = procesar_transacciones(json_file)
-
-# if clientes:
-#     generar_reporte_html(clientes)
-#     limpiarpantalla()
-#     print("Reporte generado exitosamente en 'reporte.html'.")
-#     print()
-#     for cliente in clientes.values():
-#         print(cliente)
-#         print("Transacciones:")
-#         for transaccion in cliente.transacciones:
-#             print(transaccion)
-#         print("\n")
-    
-#     input("Presione Enter para salir...")
-# else:
-#     print("No se pudo procesar el archivo JSON.")
-
 import json
 from datetime import datetime
 from clases import ClienteClassic, ClienteBlack, ClienteGold, Transaccion, CajaAhorroPesos, CajaAhorroDolares
 import os
-
-class JsonValidationError(Exception):
-    pass
-
-class TransactionError(Exception):
-    pass
 
 def limpiarpantalla():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -146,12 +12,15 @@ def validar_json(file_path):
         with open(file_path) as f:
             return json.load(f)
     except ValueError as e:
-        raise JsonValidationError(f"Error en el archivo JSON: {e}")
+        print(f"Error en el archivo JSON: {e}")
+        return None
 
 # Proceso transacciones del json y las guardo en los clientes.
 def procesar_transacciones(json_file):
     data = validar_json(json_file)
-    
+    if not data:
+        return None
+
     clientes = {}
     
     # Crear los clientes y asignarles transacciones
@@ -182,10 +51,7 @@ def procesar_transacciones(json_file):
             fecha_transaccion = datetime.strptime(trans["fecha"], "%d/%m/%Y %H:%M:%S")
             nueva_transaccion = Transaccion(trans["tipo"], trans["monto"], fecha_transaccion, trans["estado"])
             # Mediante esa funcion se le asigna estado : aprobado / rechazado
-            try:
-                validar_transaccion(clientes[cliente_numero], nueva_transaccion, trans)
-            except TransactionError as e:
-                print(f"Error al procesar la transacción: {e}")
+            validar_transaccion(clientes[cliente_numero], nueva_transaccion, trans)
     
     return clientes
 
@@ -193,46 +59,33 @@ def procesar_transacciones(json_file):
 def validar_transaccion(cliente, transaccion, data_transaccion):
     tipo = transaccion.tipo
     monto = transaccion.monto
-
-    # Asegúrate de que el monto sea positivo
-    if monto < 0:
-        raise TransactionError("No se puede procesar una transacción con un monto negativo.")
-
-    # Procesamiento de retiros en cajero
+    
+    # Ejemplo de procesamiento de retiros en cajero
     if tipo == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO":
         cuenta = cliente.cajaDeAhorroPesos 
-        resultado = cliente.retirar_dinero(cuenta, monto)
-        if resultado == 1:
+        if cliente.retirar_dinero(cuenta, monto) == 1:
             transaccion.estado = "ACEPTADA"
         else:
             transaccion.estado = "RECHAZADA"
-            raise TransactionError(resultado)
-
+            transaccion.razon = cliente.retirar_dinero(cuenta, monto)
+    
     # Procesar transferencias
     elif tipo == "TRANSFERENCIA_ENVIADA":
-        if isinstance(cliente, ClienteClassic):
-            raise TransactionError("Los clientes Classic no pueden realizar transferencias.")
-        
         cliente_destino = buscar_cliente_destino(data_transaccion["cuentaDestino"], cliente)  
-        resultado = cliente.realizar_transferencia(cliente_destino, monto)
-        if resultado == 1:
+        if cliente.realizar_transferencia(cliente_destino, monto) == 1:
             transaccion.estado = "ACEPTADA"
         else:
             transaccion.estado = "RECHAZADA"
-            raise TransactionError(resultado)
-
-    # Procesar altas de tarjetas
+            transaccion.razon = cliente.realizar_transferencia(cliente_destino, monto)
+    
+    ## Procesar altas de tarjetas
     elif tipo == "ALTA_TARJETA_CREDITO":
-        if isinstance(cliente, ClienteClassic):
-            raise TransactionError("No puede tener tarjeta de crédito siendo cliente Classic.")
-        
-        resultado = cliente.alta_tarjeta_credito(data_transaccion["numeroTarjeta"])
-        if resultado == 1:
+        if cliente.alta_tarjeta_credito(data_transaccion["numeroTarjeta"]) == 1:
             transaccion.estado = "ACEPTADA"
         else:
             transaccion.estado = "RECHAZADA"
-            raise TransactionError(resultado)
-
+            transaccion.razon = cliente.alta_tarjeta_credito(data_transaccion["numeroTarjeta"])
+    
     cliente.agregar_transaccion(transaccion)
 
 # Ejemplo de función para buscar el cliente destino en las transferencias
@@ -244,34 +97,31 @@ def buscar_cliente_destino(numero_cuenta, cliente):
 def generar_reporte_html(clientes):
     with open("reporte.html", "w") as f:
         f.write("<html><body><h1>Reporte de Transacciones</h1><table border='1'>")
-        f.write("<tr><th>Cliente</th><th>Transacción</th><th>Estado</th><th>Monto</th><th>Fecha</th><th>Razón</th></tr>")
+        f.write("<tr><th>Cliente</th><th>Transaccion</th><th>Estado</th><th>Monto</th><th>Fecha</th><th>Razon</th></tr>")
         
         for cliente in clientes.values():
             for trans in cliente.transacciones:
-                razon = trans.razon if hasattr(trans, 'razon') and trans.estado == "RECHAZADA" else ""
+                razon = trans.razon if trans.estado == "RECHAZADA" else ""
                 f.write(f"<tr><td>{cliente.nombre} {cliente.apellido}</td><td>{trans.tipo}</td><td>{trans.estado}</td><td>{trans.monto}</td><td>{trans.fecha}</td><td>{razon}</td></tr>")
         
         f.write("</table></body></html>")
 
 json_file = "transacciones.json"
 
-try:
-    clientes = procesar_transacciones(json_file)
+clientes = procesar_transacciones(json_file)
 
-    if clientes:
-        generar_reporte_html(clientes)
-        limpiarpantalla()
-        print("Reporte generado exitosamente en 'reporte.html'.")
-        print()
-        for cliente in clientes.values():
-            print(cliente)
-            print("Transacciones:")
-            for transaccion in cliente.transacciones:
-                print(transaccion)
-            print("\n")
-        
-        input("Presione Enter para salir...")
-    else:
-        print("No se pudo procesar el archivo JSON.")
-except JsonValidationError as e:
-    print(e)
+if clientes:
+    generar_reporte_html(clientes)
+    limpiarpantalla()
+    print("Reporte generado exitosamente en 'reporte.html'.")
+    print()
+    for cliente in clientes.values():
+        print(cliente)
+        print("Transacciones:")
+        for transaccion in cliente.transacciones:
+            print(transaccion)
+        print("\n")
+    
+    input("Presione Enter para salir...")
+else:
+    print("No se pudo procesar el archivo JSON.")

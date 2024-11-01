fd7c926e7f06ff4eebc1453008a4b9dd25ff36f4
Manuel Krivoy <krivoymanuel@gmail.com>
2024-11-01 11:50:44 -0300

    arreglo de querys en general.

--- backend/sprint_6/problematica_2.sql
+++ backend/sprint_6/problematica_2.sql
@@ -1,15 +1,21 @@
 -- Segunda Problematica
 
 CREATE VIEW vista_cliente AS
-SELECT customer_id, branch_id, customer_name, customer_surname, customer_DNI,
-       (strftime('%Y', 'now') - strftime('%Y', customer_dob)) AS edad
-FROM Cliente;
+    SELECT 
+        customer_id, 
+        branch_id, 
+        customer_name, 
+        customer_surname, 
+        customer_DNI,
+        (strftime('%Y', 'now') - strftime('%Y', dob)) - 
+        (strftime('%m-%d', 'now') < strftime('%m-%d', dob)) AS edad
+    FROM cliente;
 
 SELECT * FROM vista_cliente WHERE edad > 40 ORDER BY customer_DNI;
 
 SELECT * FROM vista_cliente WHERE customer_name IN ('Anne', 'Tyler') ORDER BY edad;
 
-INSERT INTO Cliente (customer_name, customer_surname, customer_DNI, branch_id, customer_dob) VALUES
+INSERT INTO Cliente (customer_name, customer_surname, customer_DNI, branch_id,dob) VALUES
 ('Lois', 'Stout', '47730534', 80, '1984-07-07'),
 ('Hall', 'Mcconnell', '52055464', 45, '1968-04-30'),
 ('Hilel', 'Mclean', '43625213', 77, '1993-03-28'),
@@ -17,7 +23,6 @@ INSERT INTO Cliente (customer_name, customer_surname, customer_DNI, branch_id, c
 ('Gabriel', 'Harmon', '57063950', 27, '1976-04-01');
 SELECT * FROM Cliente 
 ORDER BY customer_id DESC LIMIT 5;
-
 
 UPDATE Cliente SET branch_id = 10 WHERE customer_DNI IN ('47730534', '52055464', '43625213', '21207908', '57063950');
 
--- backend/sprint_6/problematica_4.sql
+++ backend/sprint_6/problematica_4.sql
@@ -6,7 +6,55 @@ JOIN Cliente c ON s.branch_id = c.branch_id
 GROUP BY s.branch_name 
 ORDER BY cantidad_clientes DESC;
 
--- falta agregar por empleado / tarjeta
+-- Cantidad de empleados por cliente por sucursal
+SELECT 
+    s.branch_id, 
+    s.branch_name, 
+    COALESCE(e.emp_count, 0) AS 'Cantidad empleados', -- SI no hay empleados asigna 0
+    COALESCE(c.cust_count, 0) AS 'Cantidad Clientes',
+    CASE WHEN COALESCE(c.cust_count, 0) > 0 THEN COALESCE(e.emp_count, 0) / COALESCE(c.cust_count, 0) -- Esto evita que no haya problemas en divisones por 0
+         ELSE NULL
+    END AS 'Cantidad de empleados por cliente'
+FROM 
+    sucursal s
+LEFT JOIN 
+    (SELECT branch_id, COUNT(employee_id) AS emp_count FROM empleado GROUP BY branch_id) e 
+ON s.branch_id = e.branch_id
+LEFT JOIN 
+    (SELECT branch_id, COUNT(customer_id) AS cust_count FROM cliente GROUP BY branch_id) c 
+ON s.branch_id = c.branch_id;
+
+-- Obtener la cantidad de tarjetas de crédito por tipo por sucursal
+SELECT 
+    s.branch_id,
+    s.branch_name,
+    COUNT(t.card_id) AS 'Cantidad de tarjetas de crédito'
+FROM
+    sucursal s
+LEFT JOIN 
+    cliente c ON c.branch_id = s.branch_id
+LEFT JOIN
+    tarjeta t ON c.customer_id = t.customer_id
+LEFT JOIN
+    Tipo_Tarjeta tt ON tt.card_type_id = t.tipo
+WHERE 
+    tt.card_type_name = 'credito'
+GROUP BY
+    s.branch_id, s.branch_name;
+
+-- Obtener el promedio de créditos otorgado por sucursal
+SELECT 
+    s.branch_id,
+    s.branch_name,
+    AVG(p.loan_total) AS 'Promedio de crédito otorgado'
+FROM 
+    sucursal s
+LEFT JOIN 
+    cliente c ON c.branch_id = s.branch_id
+LEFT JOIN 
+    prestamo p ON c.customer_id = p.customer_id
+GROUP BY 
+    s.branch_id, s.branch_name;
 
 CREATE TABLE auditoria_cuenta (
     old_id INTEGER,

d56dde81da01085967f216610f4a4a760a0843ed
Manuel Krivoy <krivoymanuel@gmail.com>
2024-11-01 12:29:50 -0300

    finalización sprint 6

--- backend/sprint_6/problematica_4.sql
+++ backend/sprint_6/problematica_4.sql
@@ -1,5 +1,6 @@
 -- Cuarta Problematica
 
+-- Cantidad de clientes por sucursal
 SELECT s.branch_name, COUNT(c.customer_id) AS cantidad_clientes 
 FROM Sucursal s 
 JOIN Cliente c ON s.branch_id = c.branch_id 
@@ -61,17 +62,75 @@ CREATE TABLE auditoria_cuenta (
     new_id INTEGER,
     old_balance REAL,
     new_balance REAL,
-    old_iban TEXT,
-    new_iban TEXT,
-    old_type TEXT,
-    new_type TEXT,
+    old_account_id INTEGER,
+    new_account_id INTEGER,
+    old_type INTEGER,
+    new_type INTEGER,
     user_action TEXT,
     created_at TEXT DEFAULT (DATETIME('now'))
 );
 
 CREATE TRIGGER after_update_cuenta
-AFTER UPDATE OF balance ON Cuenta
+AFTER UPDATE OF balance, iban, tipo ON Cuenta
 BEGIN
-    INSERT INTO auditoria_cuenta (old_id, new_id, old_balance, new_balance, user_action, created_at)
-    VALUES (old.account_id, new.account_id, old.balance, new.balance, 'update', DATETIME('now'));
+    INSERT INTO auditoria_cuenta (
+        old_id, 
+        new_id, 
+        old_balance, 
+        new_balance, 
+        old_account_id, 
+        new_account_id, 
+        old_type, 
+        new_type, 
+        user_action, 
+        created_at
+    )
+    VALUES (
+        old.account_id, 
+        new.account_id, 
+        old.balance, 
+        new.balance, 
+        old.account_id, 
+        new.account_id, 
+        old.account_type_id, 
+        new.account_type_id, 
+        'update', 
+        DATETIME('now')
+    );
 END;
+
+UPDATE Cuenta 
+SET balance = balance - 100 
+WHERE account_id IN (10, 11, 12, 13, 14);
+
+CREATE INDEX idx_cliente_dni ON Cliente (customer_dni);
+
+CREATE TABLE movimientos (
+    movimiento_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    account_id INTEGER,
+    monto REAL,
+    tipo_operacion TEXT,
+    hora TEXT DEFAULT (DATETIME('now'))
+);
+
+BEGIN TRANSACTION;
+-- Restar $1000 de la cuenta 200
+UPDATE Cuenta 
+SET balance = balance - 1000 
+WHERE account_id = 200;
+
+-- Sumar $1000 a la cuenta 400
+UPDATE Cuenta 
+SET balance = balance + 1000 
+WHERE account_id = 400;
+
+-- Registrar el movimiento en la tabla movimientos para la cuenta 200
+INSERT INTO movimientos (account_id, monto, tipo_operacion)
+VALUES (200, -1000, 'transferencia');
+
+-- Registrar el movimiento en la tabla movimientos para la cuenta 400
+INSERT INTO movimientos (account_id, monto, tipo_operacion)
+VALUES (400, 1000, 'transferencia');
+
+
+COMMIT;

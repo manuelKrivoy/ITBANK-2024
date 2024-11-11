d1e5f6a3ada07af0e6eba71f0ec2846895cc79ad
Manuel Krivoy <krivoymanuel@gmail.com>
2024-11-11 12:01:32 -0300

    genero primera vista

--- backend/sprint_7/clientes/views.py
+++ backend/sprint_7/clientes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from .models import Cliente
 
-# Create your views here.
+class IndexView(generic.ListView):
+    model = Cliente
+    template_name = "clientes/index.html"
+    context_object_name = "clientes"  # Esto le da el nombre "clientes" al contexto en la plantilla

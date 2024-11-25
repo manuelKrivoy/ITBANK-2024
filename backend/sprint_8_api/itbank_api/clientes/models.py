c5e0b02251ab490c1c9523ef9ae953e6edf18cf5
Manuel Krivoy <krivoymanuel@gmail.com>
2024-11-25 19:47:28 -0300

    agrego campo password y email a cliente para que pueda validar el logeo

--- backend/sprint_8_api/itbank_api/clientes/models.py
+++ backend/sprint_8_api/itbank_api/clientes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 class Direccion (models.Model): ## Entidad Dirección.
     calle = models.CharField(max_length=100)
@@ -24,14 +25,21 @@ class Sucursal(models.Model): ## Entidad Sucursal.
         return f'{self.nombre} - {self.direccion}'
 
 class Cliente(models.Model): ## Entidad Cliente.
-    nombre = models.CharField(max_length=100)
-    apellido= models.CharField(max_length=100)
-    dni = models.CharField(max_length=10)
-    fecha_nacimiento = models.DateField()
+    nombre = models.CharField(max_length=100, default='')
+    apellido = models.CharField(max_length=100, default='')
+    dni = models.CharField(max_length=10, unique=True, default='')
+    fecha_nacimiento = models.DateField(default='1900-01-01')
     direcciones = models.ManyToManyField(Direccion) ## Many to Many para permitir muchas direcciones al mismo cliente
     tipo = models.ForeignKey(TipoCliente, on_delete=models.CASCADE)
-    usd= models.FloatField(default=0)
-    pesos= models.FloatField(default=0)
+    usd = models.FloatField(default=0)
+    pesos = models.FloatField(default=0)
+    email = models.EmailField(unique=True, default= '')
+    password = models.CharField(max_length=100, default='1234' )
+
+    def save(self, *args, **kwargs):
+        # Hashea la contraseña antes de guardar
+        self.password = make_password(self.password)
+        super(Cliente, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.nombre} - {self.apellido} - {self.dni} - {self.fecha_nacimiento} - {self.tipo}'
--- backend/sprint_8_api/itbank_api/clientes/views.py
+++ backend/sprint_8_api/itbank_api/clientes/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import Cliente
 from .serializers import ClienteSerializer
 
-class ClienteViewSet(viewsets.ModelViewSet):
+class ClienteViewSet(viewsets.ModelViewSet): ## Model View Set sirve para hacer un CRUD de la entidad Cliente.
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
